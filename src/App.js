4353253b5fce31e1ab7d9e86da2d9d40fbf00f0b
Pranjal Bareth <pranjalnirmala@gmail.com>
2022-05-17 21:28:36 +0000

    added:meme-component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Header from "./components/Header"
+import Meme from "./components/Meme"
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-    </div>
-  );
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Meme />
+        </div>
+    )
 }
-
-export default App;

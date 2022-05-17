e9e33b89fd6f84ee74324f7d9e66bac85c0ed756
Pranjal Bareth <pranjalnirmala@gmail.com>
2022-05-17 21:40:03 +0000

    added:forms-n-meme

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,7 +1,31 @@
+import React from "react"
+
 export default function Meme() {
+   
     return (
-        <header>
-            <h1>Meme component</h1>
-        </header>
+        <main>
+            <div className="form">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                />
+                <button 
+                    className="form--button"
+                >
+                    Get a new meme image 🖼
+                </button>
+            </div>
+            <div className="meme">
+                <img src="http://i.imgflip.com/1bij.jpg" className="meme--image" />
+                <h2 className="meme--text top">one does not simply</h2>
+                <h2 className="meme--text bottom">walk into mordor</h2>
+            </div>
+        </main>
     )
 }

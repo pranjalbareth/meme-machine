e18d3970099f2b06d9a9ba7b0cc963ba27de4460
Pranjal Bareth <pranjalnirmala@gmail.com>
2022-05-17 21:36:41 +0000

    styled:header-component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
+import React from "react"
 export default function Header() {
     return (
-        <header>
-            <h1>Header component</h1>
+        <header className="header">
+            <img 
+                src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
+                className="header--image"
+                alt="MG"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">PranjalBareth</h4>
         </header>
     )
 }
